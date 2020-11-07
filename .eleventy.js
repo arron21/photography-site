e8ef1906878ce6b826f757c73b64150181691066
arron <arron@concourselabs.com>
2020-11-06 17:36:49 -0800

    site now auto builds gallery added glightbox added masonry

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,21 @@
 const fg = require("fast-glob");
+const mkdirp = require('mkdirp')
+const sharp = require('sharp');
+
+
+const galleryImagesA = fg(["**/gallery/**", "!**/_site", "!**/thumb"]).then(x => {
+  console.log(x)
+  buildThumbnails(x)
+});
 // Run search for images in /gallery
-const galleryImages = fg.sync(["**/gallery/*", "!**/_site"]);
+const galleryImages = fg.sync(["gallery/**/*", "!**/_site", "!**/_thumb"]);
+
+
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("gallery");
+  eleventyConfig.addPassthroughCopy("thumb");
+  eleventyConfig.addPassthroughCopy("assets");
 
 
 
@@ -12,3 +24,21 @@ module.exports = function (eleventyConfig) {
     return galleryImages;
   });
 };
+
+
+function buildThumbnails(thumnailArray) {
+
+
+  for(const img of thumnailArray) {
+    let imgStr = img.toString()
+    imgStr = imgStr.split('/');
+
+    imgStr.pop();
+    imgStr = imgStr.join('/');
+    const made = mkdirp.sync('thumb/'+imgStr)
+
+    sharp(img)
+      .resize({ width: 250 })
+      .toFile('thumb/'+img)
+  }
+}
